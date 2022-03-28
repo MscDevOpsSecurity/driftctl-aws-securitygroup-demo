7e3b1eb1fa3d205b9c6b0cfd46770c9e3f275c3a
juanjoSanz <juanjo.sanzmartin@gmail.com>
2022-03-28 22:00:07 +0200

    add region

--- tf/providers.tf
+++ tf/providers.tf
@@ -1,7 +1,15 @@
+# Terraform Block
 terraform {
-  required_version = "= 0.14.10"
+/*  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx */
   required_providers {
-    aws    = "= 3.36.0"
-    random = "= 3.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3"
+    }
   }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
 }
